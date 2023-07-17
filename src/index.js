276a3211200272f3c67916dcc36374bbae4f20a7
Muhammad <121952755+myusuf4@users.noreply.github.com>
2023-07-17 13:37:37 +0500

    2dars

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-
+import './index.css';
+const student=[
+  {id:1,name:"Muhammad"},
+  {id:2,name:"Muhammad"},
+  {id:3,name:"Muhammad"},
+  {id:4,name:"Muhammad"},
+  {id:5,name:"Muhammad"},
+  {id:6,name:"Muhammad"},
+]
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {student.map((v)=>{
+      return <App data={v}/>
+    })}
+    
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
