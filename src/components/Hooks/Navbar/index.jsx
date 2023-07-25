629027cc596b59e11b35b7ebb26b5b124bc5174f
Muhammad <121952755+myusuf4@users.noreply.github.com>
2023-07-25 14:11:28 +0500

    Context Api

--- src/components/Hooks/Navbar/index.jsx
+++ src/components/Hooks/Navbar/index.jsx
@@ -1,9 +1,13 @@
 import { Container } from "./style";
+import React,{useContext} from "react";
+import { StudentsContext } from "../../context";
 
-const Navbar=({student})=>{
+const Navbar=()=>{
+  const [student]=useContext(StudentsContext);
+
   return(
     <Container >
-      <h1>Students{student.length}</h1>
+      <h1>Students ({student.length})</h1>
       <h1>Home</h1>
       <h1>Properties</h1>
       <h1>Contacts</h1>
